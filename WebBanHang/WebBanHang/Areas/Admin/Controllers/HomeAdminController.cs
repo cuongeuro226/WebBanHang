dcad146fb19887704996f5fed4269ac10b4dc9ea
nhanlnhth <nhanlnhth@gamail.com>
2016-03-23 15:56:22 +0700

    add area admin

---
+++ WebBanHang/WebBanHang/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: Admin/HomeAdmin
+        // GET: Admin/HomeAdmin
+        public ActionResult Index()
+        {
+            IEnumerable<Product> model = new Products().GetAll();
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Edit(String id)
+        {
+            return View();
+        }    }
+}
---
+++ WebBanHang/WebBanHang/Models/Products.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHang.Models
+{
+    
+
+    public class Products
+    {
+        ShopWallpaperEntities db;
+
+        public Products()
+        {
+            db = new ShopWallpaperEntities();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products.ToList();
+        }
+
+        public  Product GetById(long id)
+        {
+            return db.Products.Where(m => m.Id == id).SingleOrDefault();
+        }
+
+        public int Insert( Product obj)
+        {
+            db.Products.Add(obj);
+            return db.SaveChanges();
+
+        }
+
+        public int Delete( Product obj)
+        {
+            db.Products.Remove(obj);
+            return db.SaveChanges();
+
+        }
+
+
+    }
+}
